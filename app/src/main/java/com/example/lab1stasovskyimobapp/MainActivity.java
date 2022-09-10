d661e87d7522ac470abc2f4c38264e369556fd90
Bohdan <https://github.com/BohdanStasov/lab1mobapp>
2022-09-10 20:20:23 +0300

    First button

--- app/src/main/java/com/example/lab1stasovskyimobapp/MainActivity.java
+++ app/src/main/java/com/example/lab1stasovskyimobapp/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.lab1stasovskyimobapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView1);
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("Text changed! Congrats!");
+            }
+        });
     }
 }
