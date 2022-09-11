1d82282dd7238268776e4d686dd3422524bc9db3
Bohdan <https://github.com/BohdanStasov/lab1mobapp>
2022-09-11 12:50:00 +0300

    Second Button

--- app/src/main/java/com/example/lab1stasovskyimobapp/MainActivity.java
+++ app/src/main/java/com/example/lab1stasovskyimobapp/MainActivity.java
@@ -2,25 +2,39 @@ package com.example.lab1stasovskyimobapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     TextView tv;
     Button btn;
+    Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.textView1);
         btn = (Button) findViewById(R.id.button);
+        btn2 = (Button) findViewById(R.id.button2);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 tv.setText("Text changed! Congrats!");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+
+                int color = Color.argb(255, random.nextInt(265), random.nextInt(265), random.nextInt(265));
+                tv.setTextColor(color);
             }
         });
     }
